76dd7230d3a8e4ab7468c6400086a20479a0747a
omarsagoo <om.sagoo@gmail.com>
2019-09-02 15:42:05 -0700

    create mark_completed function

--- checklist.py
+++ checklist.py
@@ -29,12 +29,23 @@ def test():
     destroy(1)
 
     print(read(0))
-    print(read(1))
+
+    list_all_items()
+
+    mark_completed(0)
 
 def list_all_items():
     index = 0
     for list_item in checklist:
-        print(index + list_item)
+        print("{} {}".format(index, list_item))
         index += 1 
 
+def mark_completed(index):
+    update(index, '{} {}'.format('√', read(index)))
+    print(read(index))
+
 test()
+
+#def select(function_code):
+    
+
