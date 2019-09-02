1d6b22724b4e604e2319c53f12e27c51ff5d7639
omarsagoo <om.sagoo@gmail.com>
2019-09-02 14:52:18 -0700

    create basic CRUD functions

--- checklist.py
+++ checklist.py
@@ -1,10 +1,10 @@
 print("hello world!")
 
-checklist = list()
-checklist.append('Blue')
-print(checklist)
-checklist.append('Orange')
-print(checklist)
+# checklist = list()
+# checklist.append('Blue')
+# print(checklist)
+# checklist.append('Orange')
+# print(checklist)
 
 #create
 def create(item):
@@ -14,10 +14,30 @@ def create(item):
 def read(index):
     return checklist[index]
 
-checklist = ['Blue', 'Orange']
-checklist[1] = 'Cats'
-print(checklist)
+# checklist = ['Blue', 'Orange']
+# checklist[1] = 'Cats'
+# print(checklist)
 
 #update
 def update(index, item):
     checklist[index] = item
+
+#remove
+def destroy(index):
+    checklist.pop(index)
+
+#testing
+def test():
+    create("purple sox")
+    create("red cloak")
+
+    print(read(0))
+    print(read(1))
+
+    update(0, "purple socks")
+    destroy(1)
+
+    print(read(0))
+    print(read(1))
+
+test()
